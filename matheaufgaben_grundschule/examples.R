0c349ff805697d3f0fafd3bcdfba68a4d433bd0a
Ivan Kondofersky <Ivan@OlchingMacbook.fritz.box>
2020-05-01 22:35:07 +0200

    Beispiele ergänzt um Plot Beispiel

--- matheaufgaben_grundschule/examples.R
+++ matheaufgaben_grundschule/examples.R
@@ -3,3 +3,10 @@ generate_exercise_list(Nexercise = 10, difficulty = 2, type = 1)
 generate_exercise_list(Nexercise = 10, difficulty = 2, type = 2)
 
 # Test plotting
+ex1 <- generate_exercise_list(Nexercise = 20, difficulty = 3, type = 2)
+plot(ex1, base_size = 10)
+
+pdf(paste0('matheaufgaben_grundschule/example_output/example_', Sys.time(), '.pdf'), paper = 'a4')
+plot(ex1)
+dev.off()
+
