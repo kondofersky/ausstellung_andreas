6d4e353f14b815c2476a8a5bd41fd2f835875626
Ivan Kondofersky <Ivan@OlchingMacbook.fritz.box>
2020-05-09 21:13:54 +0200

    Fix für Dateiname

--- matheaufgaben_grundschule/examples.R
+++ matheaufgaben_grundschule/examples.R
@@ -6,7 +6,7 @@ generate_exercise_list(Nexercise = 10, difficulty = 2, type = 2)
 ex1 <- generate_exercise_list(Nexercise = 20, difficulty = 3, type = 2)
 plot(ex1, base_size = 10)
 
-pdf(paste0('matheaufgaben_grundschule/example_output/example_', Sys.time(), '.pdf'), paper = 'a4')
+pdf(paste0('matheaufgaben_grundschule/example_output/example_', format(Sys.time(), "%Y-%m-%d-%H-%M-%S"), '.pdf'), paper = 'a4')
 plot(ex1)
 dev.off()
 
