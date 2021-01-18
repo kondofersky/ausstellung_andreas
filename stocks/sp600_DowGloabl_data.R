e8747bba381d8c3a0e417ed208a7e6d105b7d613
sschiwek <steffen.schiwek@gmx.de>
2021-01-18 20:00:44 +0100

    10 years, not 5

--- stocks/sp600_DowGloabl_data.R
+++ stocks/sp600_DowGloabl_data.R
@@ -5,7 +5,7 @@ wd.cur <- getwd()
 # fetch all stocks from 's&p600'
 sp600 <- tq_index('SP600')%>%mutate(index='SP600')
 
-# fetch data for last 5 years for s&p600-indices
+# fetch data for last 10 years for s&p600-indices
 mysp600data <- tq_get(sp600$symbol,
                       from = Sys.Date() - (365*10))%>%
   right_join(sp600,.)
@@ -13,7 +13,7 @@ mysp600data <- tq_get(sp600$symbol,
 # fetch all stocks from 'dowglobal'
 dowglobal <- tq_index('DOWGLOBAL')%>%mutate(index='DOWGlobal')
 
-# fetch data for last 5 years for dowglobal-indices
+# fetch data for last 10 years for dowglobal-indices
 
 mydowglobaldata <- tq_get(dowglobal$symbol,
                       from = Sys.Date() - (365*10))%>%
